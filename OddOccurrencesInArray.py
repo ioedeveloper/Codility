66455050a504ea5e14dc275acd844c521cb01fb7
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-10-23 15:39:52 +0100

    Odd Occurences In Array

--- OddOccurrencesInArray.py
+++ OddOccurrencesInArray.py
@@ -1,12 +1,13 @@
 def solution(A):
     # write your code in Python 3.6
-    visited = []
-    for i in range(len(A)):
-        if i not in visited:
-            if((i+2) < len(A)):
-                if(A[i+2] != A[i]):
-                    return A[i]
-                visited.append(A[i])
-                visited.append(A[i+2])
-            else:
+    A.sort()
+    if(len(A) == 1):
+        return A[0]
+    for i in range(0, len(A), 2): 
+        if((i+1) < len(A)):
+            if(A[i+1] != A[i]):
                 return A[i]
+        else:
+            return A[i]
+
+print(solution([9, 3, 9, 3, 9, 7, 9]))
